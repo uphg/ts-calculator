40321c30fdc26b0d6730bba67b7087db6b09a8bd
lvheng <1834805770@qq.com>
2021-05-01 09:10:54 +0800

    测试打包功能

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,162 @@
-(()=>{"use strict";var t={379:(t,e)=>{Object.defineProperty(e,"__esModule",{value:!0});var r=function(t,e,r){return void 0===e&&(e="number"),void 0===r&&(r=t),{name:t,type:e,text:r}},a=[[r("clear","operator","Clear"),r("divide","operator","÷")],[r("7"),r("8"),r("9"),r("multiply","operator","×")],[r("4"),r("5"),r("6"),r("subtract","operator","-")],[r("1"),r("2"),r("3"),r("add","operator","+")],[r("0"),r("point","number","."),r("equal","operator","=")]];e.default=a}},e={};function r(a){var n=e[a];if(void 0!==n)return n.exports;var o=e[a]={exports:{}};return t[a](o,o.exports,r),o.exports}(()=>{var t=r(379);function e(t,e){var r=document.createElement(t);if(e)for(var a in e)Object.hasOwnProperty.call(e,a)&&(r[a]=e[a]);return r}var a=e("div",{id:"container"});document.body.appendChild(a);var n=e("div",{className:"output"}),o=e("span",{textContent:"0",className:"output-span"});n.appendChild(o),a.appendChild(n),a.addEventListener("click",(function(t){if(console.log("event.target"),console.log(t.target),t.target instanceof HTMLButtonElement){var e=t.target.textContent;"0123456789".indexOf(e)}})),t.default.forEach((function(t){var r=e("div",{className:"row"});t.forEach((function(t){var a=e("button",{className:"button text-"+t.name,textContent:t.text});a.setAttribute("data-type",t.type),r.appendChild(a)})),a.appendChild(r)}))})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.buttonText = void 0;
+var buttonText;
+(function (buttonText) {
+    buttonText["clear"] = "clear";
+    buttonText["divide"] = "divide";
+    buttonText["multiply"] = "multiply";
+    buttonText["subtract"] = "subtract";
+    buttonText["add"] = "add";
+    buttonText["point"] = "point";
+    buttonText["equal"] = "equal";
+})(buttonText = exports.buttonText || (exports.buttonText = {}));
+
+
+/***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var enum_1 = __webpack_require__(1);
+var createData = function (name, type, text) {
+    if (type === void 0) { type = 'number'; }
+    if (text === void 0) { text = name; }
+    return ({ name: name, type: type, text: text });
+};
+var buttons = [
+    [
+        createData(enum_1.buttonText.clear, 'operator', 'Clear'),
+        createData(enum_1.buttonText.divide, 'operator', '÷'),
+    ],
+    [
+        createData('7'),
+        createData('8'),
+        createData('9'),
+        createData(enum_1.buttonText.multiply, 'operator', '×'),
+    ],
+    [
+        createData('4'),
+        createData('5'),
+        createData('6'),
+        createData(enum_1.buttonText.subtract, 'operator', '-'),
+    ],
+    [
+        createData('1'),
+        createData('2'),
+        createData('3'),
+        createData(enum_1.buttonText.add, 'operator', '+'),
+    ],
+    [
+        createData('0'),
+        createData(enum_1.buttonText.point, 'number', '.'),
+        createData(enum_1.buttonText.equal, 'operator', '='),
+    ]
+];
+exports.default = buttons;
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var enum_1 = __webpack_require__(1);
+var buttons_1 = __webpack_require__(2);
+function createElement(labelName, attribute) {
+    var el = document.createElement(labelName);
+    if (attribute) {
+        for (var key in attribute)
+            if (Object.hasOwnProperty.call(attribute, key)) {
+                el[key] = attribute[key];
+            }
+    }
+    return el;
+}
+var beforeNumber, afterNumber, mark;
+var container = createElement('div', { id: 'container' });
+document.body.appendChild(container);
+var output = createElement('div', { className: 'output' });
+var span = createElement('span', {
+    textContent: '0',
+    className: 'output-span'
+});
+output.appendChild(span);
+container.appendChild(output);
+container.addEventListener('click', function (event) {
+    if (event.target instanceof HTMLButtonElement) {
+        var text = event.target.textContent;
+        var el = event.target;
+        var name_1 = el.getAttribute('data-name');
+        console.log("name");
+        console.log(name_1);
+        if (el.getAttribute('data-name') === enum_1.buttonText.add) {
+            console.log('添加按钮');
+        }
+        if ('0123456789'.indexOf(text) >= 0) {
+            if (mark) {
+                afterNumber = text;
+            }
+            else {
+                beforeNumber = text;
+            }
+        }
+        else if ([].indexOf(text)) {
+        }
+    }
+});
+buttons_1.default.forEach(function (item) {
+    var div = createElement('div', { className: 'row' });
+    item.forEach(function (button) {
+        var buttonEl = createElement('button', {
+            className: "button text-" + button.name,
+            textContent: button.text
+        });
+        buttonEl.setAttribute('data-type', button.type);
+        buttonEl.setAttribute('data-name', button.name);
+        div.appendChild(buttonEl);
+    });
+    container.appendChild(div);
+});
+
+})();
+
+/******/ })()
+;
